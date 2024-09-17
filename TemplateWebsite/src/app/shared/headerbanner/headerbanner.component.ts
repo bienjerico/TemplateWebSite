b7377383f618c697ddf52d48e5713a077d7b6f8b
Bien Cueto <bien.cu@softwaredesign.com.lb>
2024-09-17 23:59:32 +0400

    angular and bootstrap

---
+++ TemplateWebsite/src/app/shared/headerbanner/headerbanner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headerbanner',
+  standalone: true,
+  imports: [],
+  templateUrl: './headerbanner.component.html',
+  styleUrl: './headerbanner.component.css'
+})
+export class HeaderbannerComponent {
+
+}
